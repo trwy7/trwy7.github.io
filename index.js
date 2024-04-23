c11f520eefd6c997111e63b94a6e50980089eaf3
Trey <47907719+trwy7@users.noreply.github.com>
2024-04-23 11:17:22 -0600

    Loading text and page change rework

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ function loadPage(url) {
     if (typeof url !== 'string') {
         throw new Error('Invalid argument. Expected a string.');
     }
+    const oldurl = window.location.pathname;
     // button.innerHTML = 'Loading...';
     if (url.startsWith('/')) {
         document.getElementById('body').classList.add('leave');
@@ -24,11 +25,13 @@ function loadPage(url) {
     } else {
         document.getElementById('body').classList.add('leaveExternal');
     }
-    setTimeout(() => {
+    setTimeout(async () => {
         if (url.startsWith('/')) {
-            window.history.replaceState( {} , 'title', url );
+            let i = 0
             do {
                 if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+                    document.body.classList.remove('loading');
+                    window.history.replaceState( {} , 'title', url );
                     var parser = new DOMParser();
                     var doc = parser.parseFromString(xmlHttp.responseText, "text/html");
                     document.getElementById('outer').innerHTML = doc.getElementById('outer').innerHTML;
@@ -36,22 +39,27 @@ function loadPage(url) {
                     newpage();
                     break;
                 } else {
-                    console.error('Request did not succeed before timeout.')
-                    if (xmlHttp.status != 200) {
+                    if (xmlHttp.status != 200 && xmlHttp.readyState == 4) {
                         if (xmlHttp.status == 404) {
                             alert('Failed to connect: 404 (Page not found)');
-                            location.reload();
+                            document.body.classList.remove('leave');
+                            document.body.classList.remove('loading');
+                            console.error('404 while loading page')
                             break;
                         }
                         alert('Failed to connect: Error ' + xmlHttp.status);
+                        document.body.classList.remove('loading');
                         location.reload();
+                        console.error('Error while loading page')
                         break;
                     }
                     if (i == 49) {
+                        console.error('Request did not succeed before timeout.')
                         alert('Failed to connect');
                         location.reload();
                     }
-                    new Promise(r => setTimeout(r, 250));
+                    document.body.classList.add('loading');
+                    await new Promise(r => setTimeout(r, 250));
                     i = i + 1;
                 }
             } while (i < 50);
@@ -59,7 +67,7 @@ function loadPage(url) {
             window.location.href = url;
         }
         // window.location.href = url;
-    }, 1250);
+    }, 1000);
     
 }
 function getBrowser() {
