acbe5e2aef4e565ec1e37eaa747bdba5642166ad
Trey <47907719+trwy7@users.noreply.github.com>
2024-04-23 10:24:18 -0600

    Rework page switching

--- index.js
+++ index.js
@@ -11,27 +11,52 @@ function loadPage(url) {
         throw new Error('Invalid argument. Expected a string.');
     }
     // button.innerHTML = 'Loading...';
-    document.getElementById('body').classList.add('leave');
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.onreadystatechange = function() { 
-        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
-            console.log('Request successful');
+    if (url.startsWith('/')) {
+        document.getElementById('body').classList.add('leave');
+        var xmlHttp = new XMLHttpRequest();
+        xmlHttp.onreadystatechange = function() { 
+            if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+                console.log('Request successful');
 
+        }
+        xmlHttp.open("GET", url, true);
+        xmlHttp.send(null);
+    } else {
+        document.getElementById('body').classList.add('leaveExternal');
     }
-    xmlHttp.open("GET", url, true);
-    xmlHttp.send(null);
     setTimeout(() => {
-        window.history.replaceState( {} , 'title', url );
-        if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
-            var parser = new DOMParser();
-            var doc = parser.parseFromString(xmlHttp.responseText, "text/html");
-            document.getElementById('outer').innerHTML = doc.getElementById('outer').innerHTML;
-            document.body.classList.remove('leave');
-            newpage();
+        if (url.startsWith('/')) {
+            window.history.replaceState( {} , 'title', url );
+            do {
+                if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+                    var parser = new DOMParser();
+                    var doc = parser.parseFromString(xmlHttp.responseText, "text/html");
+                    document.getElementById('outer').innerHTML = doc.getElementById('outer').innerHTML;
+                    document.body.classList.remove('leave');
+                    newpage();
+                    break;
+                } else {
+                    console.error('Request did not succeed before timeout.')
+                    if (xmlHttp.status != 200) {
+                        if (xmlHttp.status == 404) {
+                            alert('Failed to connect: 404 (Page not found)');
+                            location.reload();
+                            break;
+                        }
+                        alert('Failed to connect: Error ' + xmlHttp.status);
+                        location.reload();
+                        break;
+                    }
+                    if (i == 49) {
+                        alert('Failed to connect');
+                        location.reload();
+                    }
+                    new Promise(r => setTimeout(r, 250));
+                    i = i + 1;
+                }
+            } while (i < 50);
         } else {
-            console.error('Request did not succeed before timeout.')
-            alert('Failed to connect');
-            location.reload();
+            window.location.href = url;
         }
         // window.location.href = url;
     }, 1250);
